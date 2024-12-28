043ed6d5094308cde0b0388919c62f1d4e7dd51c
Kan-Nifee <plasticinsurgent01@gmail.com>
2024-12-28 17:30:55 -0600

    UPDATE: now prompts for file path, includeds try and except in case of incorret path

--- main.py
+++ main.py
@@ -1,22 +1,48 @@
+#Main Purposed of this program will be to allow me to loop throw text files and evaluate them,
+#When its done I should be able to prompt for;
+#                                             1.) How many times a word is used
+#                                             2.) How many words total
+#                                             3.) Ratio of promt words used to compared to other words
+#                                             4.) Pass output of Program into text file for later reference
+
+
+
+import errno
+
 def main ():
-    path_to_file = "books/frankenstein.txt"
+#this block handles incorrect file types because humans will make errors and we dont want- 
+#-the user to have to open the file again just because I CANT TYPE lol    
+    file_not_found = False
+    while not file_not_found:
+        try:
+#variable below prompts and stores a user input so we can try and catch inorrect files and of course
+#-evaluate correct ones
+            path_to_file = (input("please provide book bot with the path to file: \n"))
+            print ("")
+            print (f"Thank you for your selection{path_to_file}")
+            print ("")
+
+            with open(path_to_file) as f:
+                file_contents = f.read()
+                file_not_found = True
+        except FileNotFoundError as e:
+            if e.errno == errno.ENOENT:
+                print ("INPUT ERROR; FILE OR PATH TO FILE IS NOT CORRECT")
     
-    with open(path_to_file) as f:
-        file_contents = f.read()
-    
-    #splits text file into whole words based on wite space for counting later.
+#splits text file into whole words based on wite space for counting later.
     words = file_contents.split()
-    #holds counted words, used to return or print total # of words in text doc.
     
+#holds counted words, used to return or print total # of words in text doc. 
     count = 0
-    #iterates over previously split words and adds them to count for tallying
+    
+#iterates over previously split words and adds them to count for tallying
     for each_word in words:
         count += 1
     
-    #NEW OPERATION for tallying idividual characters (letters) so letters can be counted
+#NEW OPERATION for tallying idividual characters (letters) so letters can be counted
     chars = list(file_contents.lower())  #Holds individual lettter from Text fil for looping throught and tallyiong
     
-    #char_count has each letter set to 0 as we iterrate over each char we will updated the count per letter then return the list)
+#char_count has each letter set to 0 as we iterrate over each char we will updated the count per letter then return the list)
     
     char_count = {
     'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0, 'f': 0, 'g': 0, 'h': 0, 'i': 0, 
@@ -28,14 +54,13 @@ def main ():
         if char in char_count:
             char_count[char] += 1
     
-    
-    
+      
+    print ("")
     
     print (f"--- Begin report of {path_to_file} ---")        
     print (f"{count} words were found in the document")
     
     for letter, value in char_count.items():
         print (f"The {letter} charactger was found {value} times")
-        
-    print ("--- End report ---")
+        print ("--- End report ---")
 main()
